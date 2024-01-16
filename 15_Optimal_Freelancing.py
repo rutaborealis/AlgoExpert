7b3136e181ec91bb65d929631ebb4efe5f487253
rutakukharenko <ruta.kukharenko@gmail.com>
2024-01-16 22:45:54 +0300

    16.01.2024 AlgoExpert problems: 14. Tandem Bicycle 15. Optimal Freelancing

---
+++ 15_Optimal_Freelancing.py
@@ -0,0 +1,44 @@
+'''
+15. Optimal Freelancing
+
+You recently started freelance software development and have been offered
+a variety of job opportunities. Each job has a deadline, meaning there is no
+value in completing the work after the deadline. Additionally, each job
+has an associated payment representing the profit for completing that job.
+Given this information, write a function that returns the maximum profit that
+can be obtained in a 7-day period.
+
+Each job will take 1 full day to complete, and the deadline will be given
+as the number of days left to complete the job. For example, if a job has a
+deadline of 1, then it can only be completed if it is the first job worked
+on. If a job has a deadline of 2, then it could be started on the first or
+second day.
+
+Note: There is no requirement to complete all of the jobs. Only one job can
+be worked on at a time, meaning that in some scenarios it will be impossible
+to complete them all.
+
+jobs = [
+  {"deadline": 1, "payment": 1},
+  {"deadline": 2, "payment": 1},
+  {"deadline": 2, "payment": 2}
+]
+'''
+
+# 1
+def optimalFreelancing(jobs):
+    LENGTH_OF_THE_WEEK = 7
+    timeline = [False] * LENGTH_OF_THE_WEEK
+    profit = 0
+
+    jobs.sort(key=lambda job: job['payment'], reverse=True)
+
+    for job in jobs:
+        max_time = min(job['deadline'], LENGTH_OF_THE_WEEK)
+        for time in reversed(range(max_time)):
+            if timeline[time] == False:
+                timeline[time] = True
+                profit += job['payment']
+                break
+
+    return profit
