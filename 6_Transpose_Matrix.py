4599c57602a5c8399fd01228052bece49597f3ac
Ruta Kukharenko <ruta.kukharenko@gmail.com>
2024-07-08 21:08:20 +0300

    08.07.2024

---
+++ 6_Transpose_Matrix.py
@@ -0,0 +1,22 @@
+'''
+6. Transpose Matrix
+
+You're given a 2D array of integers matrix. Write a function
+that returns the transpose of the matrix.
+
+The transpose of a matrix is a flipped version of the original matrix across
+its main diagonal (which runs from top-left to bottom-right); it switches
+the row and column indices of the original matrix.
+
+You can assume the input matrix always has at least 1 value; however its
+width and height are not necessarily the same.
+'''
+
+# 1
+def transposeMatrix(matrix):
+    result = [[0] * len(matrix) for _ in range(len(matrix[0]))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[j][i] = matrix[i][j]
+    return result
