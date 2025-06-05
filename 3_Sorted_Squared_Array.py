dd0c190b8e9c1a1be04515d361dd5a41a15826df
Ruta Kukharenko <ruta.kukharenko@gmail.com>
2025-06-05 20:44:32 +0300

    3. Sorted Squared Array - update

--- 3_Sorted_Squared_Array.py
+++ 3_Sorted_Squared_Array.py
@@ -9,3 +9,25 @@ of the original integers also sorted in ascending order.
 # 1
 def sortedSquaredArray(array):
     return sorted(list(map(lambda x: x**2, array)))
+
+#2
+def sortedSquaredArray(array):
+    sorted_squares = [0 for _ in array]
+    smaller_idx = 0
+    larger_idx = len(array) - 1
+
+    for idx in reversed(range(len(array))):
+        smaller_vallue = array[smaller_idx]
+        larger_value = array[larger_idx]
+
+        if abs(smaller_vallue) > abs(larger_value):
+            sorted_squares[idx] = smaller_vallue ** 2
+            smaller_idx += 1
+        else:
+            sorted_squares[idx] = larger_value ** 2
+            larger_idx -= 1
+
+    return sorted_squares
+
+
+print(sortedSquaredArray([-10, -5, 0, 5, 10]))
